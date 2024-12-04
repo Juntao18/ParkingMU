c6ef94e59cc6b328c1df1bf3d7eb1fcc784295f0
Juntao Zhai <austin.ie@yahoo.com>
2024-12-04 16:28:08 +0000

    new

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,7 @@ function Welcome() {
     <div className="welcome-container">
       <h2>Hello, welcome to Maynooth Parking Service!</h2>
       <div>
-        <a href="#">Map Mode</a>
+        <a href="#">🚗Map Mode🔄️</a>
       </div>
     </div>
   );
